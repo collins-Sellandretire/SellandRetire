e84908c4bc5c754aad8de6a14c0ed3011a80db68
collins-Sellandretire <collins_ch@hotmail.com>
2026-07-17 19:00:28 +1000

    Update brokers-fn.js

--- netlify/functions/brokers-fn.js
+++ netlify/functions/brokers-fn.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event) {
   const token = process.env.AIRTABLE_TOKEN;
   const baseId = process.env.AIRTABLE_BASE_ID;
@@ -13,24 +11,22 @@ exports.handler = async function(event) {
 
   try {
     if (id) {
-      // Single broker by ID
       const res = await fetch(
-        `https://api.airtable.com/v0/${baseId}/${encodeURIComponent(table)}/${id}`,
-        { headers: { Authorization: `Bearer ${token}` } }
+        'https://api.airtable.com/v0/' + baseId + '/' + encodeURIComponent(table) + '/' + id,
+        { headers: { Authorization: 'Bearer ' + token } }
       );
       const data = await res.json();
       return { statusCode: 200, headers, body: JSON.stringify(data) };
     } else {
-      // All active brokers sorted alphabetically by Company Name
       let records = [];
       let offset = null;
 
       do {
-        let url = `https://api.airtable.com/v0/${baseId}/${encodeURIComponent(table)}?filterByFormula={Active}=1&sort[0][field]=Company%20Name&sort[0][direction]=asc&pageSize=100`;
-        if (offset) url += `&offset=${offset}`;
+        let url = 'https://api.airtable.com/v0/' + baseId + '/' + encodeURIComponent(table) + '?filterByFormula=%7BActive%7D%3D1&sort%5B0%5D%5Bfield%5D=Company%20Name&sort%5B0%5D%5Bdirection%5D=asc&pageSize=100';
+        if (offset) url += '&offset=' + offset;
 
         const res = await fetch(url, {
-          headers: { Authorization: `Bearer ${token}` }
+          headers: { Authorization: 'Bearer ' + token }
         });
         const data = await res.json();
         if (data.records) records = records.concat(data.records);
